dda6a6ffa22fd12e11c7c6f4e2e899e1b8188f7a
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-10 18:22:26 -0500

    Add main components

---
+++ frontend/src/components/BandAdd.jsx
@@ -0,0 +1,14 @@
+export const BandAdd = () => {
+  return (
+    <>
+      <h3>Agregar Band</h3>
+      <form action="">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Nuevo nombre de banda"
+        />
+      </form>
+    </>
+  );
+};
