da807a9e98ae0fc27e16284157d089504a0c1aec
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-10 22:48:12 -0500

    Add curd events

--- backend/models/sockets.js
+++ backend/models/sockets.js
@@ -13,8 +13,31 @@ class Sockets {
       console.log("Cliente conectado");
 
       // Emitir al cliente conectado, todas las bands actuales
-
       socket.emit("current-bands", this.bandList.getBands());
+
+      // Votar por la banda
+      socket.on("votar-banda", (id) => {
+        this.bandList.increaseVotes(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      //Eliminar banda
+      socket.on("delete-banda", (id) => {
+        this.bandList.removeBand(id);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Cambiar nombre
+      socket.on("change-name-banda", ({ id, nombre }) => {
+        this.bandList.changeName(id, nombre);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
+
+      // Agregar banda
+      socket.on("new-banda", ({ nombre }) => {
+        this.bandList.addBand(nombre);
+        this.io.emit("current-bands", this.bandList.getBands());
+      });
     });
   }
 }
