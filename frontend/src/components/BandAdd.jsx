77fcdce7a624a13cdcf48d5d8935c2c2b1db41e5
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-10 22:46:37 -0500

    Add Crud socket.io

--- frontend/src/components/BandAdd.jsx
+++ frontend/src/components/BandAdd.jsx
@@ -1,12 +1,28 @@
+import { useContext, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
+
 export const BandAdd = () => {
+  const [valor, setValor] = useState("");
+  const { socket } = useContext(SocketContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (valor.trim().length > 0) {
+      socket.emit("new-banda", { nombre: valor });
+      setValor("");
+    }
+  };
+
   return (
     <>
       <h3>Agregar Band</h3>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           className="form-control"
           placeholder="Nuevo nombre de banda"
+          value={valor}
+          onChange={(ev) => setValor(ev.target.value)}
         />
       </form>
     </>
--- frontend/src/components/BandList.jsx
+++ frontend/src/components/BandList.jsx
@@ -1,21 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
+
 export const BandList = () => {
+  const [bands, setBands] = useState([]);
+  const { socket } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on("current-bands", (bands) => setBands(bands));
+    return () => socket.off("current-bands");
+  }, [socket]);
+
+  const changeInput = (event, id) => {
+    const newName = event.target.value;
+    setBands((bands) =>
+      bands.map((band) => {
+        if (band.id === id) band.name = newName;
+        return band;
+      })
+    );
+  };
+
+  const onLostFocus = (id, nombre) =>
+    socket.emit("change-name-banda", { id, nombre });
+
+  const onVote = (id) => socket.emit("votar-banda", id);
+  const onDelete = (id) => socket.emit("delete-banda", id);
+
   const CrearRows = () => {
-    return (
-      <tr>
+    return bands.map((band) => (
+      <tr key={band.id}>
         <td>
-          <button className="btn btn-primary">+1</button>
+          <button className="btn btn-primary" onClick={() => onVote(band.id)}>
+            +1
+          </button>
         </td>
         <td>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={band.name}
+            onChange={(event) => changeInput(event, band.id)}
+            onBlur={() => onLostFocus(band.id, band.name)}
+          />
         </td>
         <td>
-          <h3>15</h3>
+          <h3>{band.votes}</h3>
         </td>
         <td>
-          <button className="btn btn-danger">Borrar</button>
+          <button className="btn btn-danger" onClick={() => onDelete(band.id)}>
+            Borrar
+          </button>
         </td>
       </tr>
-    );
+    ));
   };
 
   return (
